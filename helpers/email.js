54013b18ba8b9c19e7459496f76bf4ea53c302df
Abdul Muis <muiiizabdul@gmail.com>
2022-07-19 21:12:24 +0700

    setup rabbitmq

--- helpers/email.js
+++ helpers/email.js
@@ -1,4 +1,13 @@
-const sendEmail = (to, template, templateData) => {
+const RabbitMQ = require('../utils/rabbitmq');
+
+const rabbitmq = new RabbitMQ();
+const queue = process.env.RABBITMQ_EMAIL_QUEUE;
+
+const sendEmail = async (to, template, templateData) => {
+    if (process.env.NODE_ENV !== 'test') {
+        await rabbitmq.connect(queue);
+        rabbitmq.sendMessage({ to, body: template(templateData) });
+    }
 };
 
 module.exports = {
---
+++ utils/rabbitmq.js
@@ -0,0 +1,27 @@
+const amqp = require('amqplib');
+
+class RabbitMQ {
+    constructor() {
+        this.server = process.env.RABBITMQ_SERVER;
+    }
+
+    async connect(queue) {
+        const connection = await amqp.connect(this.server);
+        const channel = await connection.createChannel();
+        channel.assertQueue(queue, { durable: true });
+        this.connection = connection;
+        this.channel = channel;
+        this.queue = queue;
+    }
+
+    sendMessage(data) {
+        this.channel.sendToQueue(this.queue, Buffer.from(JSON.stringify(data)), { persistent: true });
+    }
+
+    close() {
+        this.channel.close();
+        this.connection.close();
+    }
+}
+
+module.exports = RabbitMQ;
